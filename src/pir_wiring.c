85e277f71bfa0a9f71367e6d8a4c1c5821010952
전지원 <jiwon@computer.local>
2018-06-06 19:56:25 +0900

    add PIR wiringPi code

---
+++ src/pir_wiring.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#define PIR 4       //BCM_GPIO 23
+
+int main(void)
+{
+  if(wiringPiSetup() == -1)
+    return 1;
+
+  pinMode(PIR,INPUT);
+  for(;;)
+  {
+    int read=digitalRead(PIR);
+    if(read)
+    {
+      printf("Detection\n");
+      delay(1000);
+    }
+    else{
+      printf("Not Detected read: %d\n",read);
+      delay(1000);
+    }
+
+  }
+  return 0;
+}
