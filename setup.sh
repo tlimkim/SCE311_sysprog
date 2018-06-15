b76c2485b24169f360d32df7d071468cdf78018f
TaekLim Kim <limkim4233@gmail.com>
2018-06-15 17:26:13 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ sudo insmod ~/workspace/babycare/dev/pir/pir_dev.ko
 sudo mknod -m 666 /dev/led_dev c 240 0
 sudo mknod -m 666 /dev/pir_dev c 241 0
 
-~/workspace/babybare/src/./exec
+# ~/workspace/babybare/src/./exec
