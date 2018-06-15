dd1ff0745c0d8ed469b24dc031af34e59c027e95
limkim4233 <limkim4233@gmail.com>
2018-06-15 20:43:35 +0900

    minor modify

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 # setting the device files for Reboot
 # Developed by Taeklim Kim
 
-sudo insmod ~/workspace/babycare/dev/led/led_dev.ko
-sudo insmod ~/workspace/babycare/dev/pir/pir_dev.ko
+sudo insmod ~/workspace/babycare/client_dev/led/led_dev.ko
+sudo insmod ~/workspace/babycare/client_dev/pir/pir_dev.ko
+sudo insmod ~/workspace/babycare/serv_dev/button_dev.ko
 
 sudo mknod -m 666 /dev/led_dev c 240 0
 sudo mknod -m 666 /dev/pir_dev c 241 0
