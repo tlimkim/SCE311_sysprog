a33b2560b81eddf8d1f0e646c0e949e34e115473
limkim4233 <limkim4233@gmail.com>
2018-06-15 20:41:21 +0900

    redirection

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# setting the device files for Reboot
+# Developed by Taeklim Kim
+
 sudo insmod ~/workspace/babycare/dev/led/led_dev.ko
 sudo insmod ~/workspace/babycare/dev/pir/pir_dev.ko
 
 sudo mknod -m 666 /dev/led_dev c 240 0
 sudo mknod -m 666 /dev/pir_dev c 241 0
 
-# ~/workspace/babybare/src/./exec
